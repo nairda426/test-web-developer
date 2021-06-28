2ea5647a132fa745a6fee785b2584d2422143699
nairda426 <86614805+nairda426@users.noreply.github.com>
2021-06-29 00:44:09 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -2,16 +2,34 @@ var images = {
 	man: {
 		heading: 'Man Image',
 		description: 'Image of a man.',
-		path: 'img/man.jpg'
+		path: 'images/man.jpg'
 	},
 	wizard: {
 		heading: 'Wizard Image',
 		description: 'Image of a wizard.',
-		path: 'img/wizard.jpg'
+		path: 'images/wizard.jpg'
 	},
 	beast: {
 		heading: 'Beast Image',
 		description: 'Image of a beast.',
-		path: 'img/beast.jpg'
+		path: 'images/beast.jpg'
 	}
 };
+
+function setManImage()
+{
+	document.getElementById("man").src
+	= ""+images.man.path+""
+}
+
+function setWizardImage()
+{
+	document.getElementById("wizard").src
+	= ""+images.wizard.path+""
+}
+
+function setBeastImage()
+{
+	document.getElementById("beast").src
+	= ""+images.beast.path+""
+}
